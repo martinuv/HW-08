683bc684cc6097f17d858b276ff4d5ce01f77a0f
nozakis <nozakis@carleton.edu>
2016-02-04 22:43:23 -0600

    Fixed computeSimilarity

--- detectAuthor.py
+++ detectAuthor.py
@@ -254,9 +254,12 @@ def computeSimilarity(signature1, signature2, weights):
     the list is ignored.
     '''
     similarity = 0
-    for i in range (1, len(weights)):
+    for i in range (1, 5):
         similarity += abs(signature1[i] - signature2[i]) * weights[i]
         
+    for i in range (4):
+        similarity += abs(signature1[6][i] - signature2[6][i]) * weights[6]
+    
     return similarity
 
 
